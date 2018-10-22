615470c0d03065c152a93eaaee28757da356f352
kurisu <callmekurisu@gmail.com>
2018-10-21 19:11:53 -0500

    added news api

---
+++ fetchNews.js
@@ -0,0 +1,34 @@
+'use strict'
+
+document.addEventListener("DOMContentLoaded", () => {
+    let newsBtn = document.getElementById("newsBtn");
+    let q = document.getElementById("query").value;
+    let desc = document.getElementById("description");
+    let newsurl = document.getElementById("newsurl");
+
+    newsBtn.addEventListener("click", event => {
+        let apiUrl = "https://newsapi.org/v2/everything?q=";
+        let q = document.getElementById("query").value;
+        let apiUrl2 = "&language=en&from=2018-10-18&sortBy=publishedAt&";
+        let apiKey = "apiKey=e5de842e9935484f8af9dd1e39123056";
+        
+        fetch(`${apiUrl}${q}${apiUrl2}${apiKey}`)
+        
+        //.json() method returns a Promise
+        //of the response body parsed from JSON
+        .then(res => res.json())
+        .then(data => {
+            //return random results of news articles
+            let numArticles = Object.keys(data.articles).length;
+            let n = Math.floor(Math.random() * numArticles);
+            news.innerHTML = data.articles[n].title;
+            desc.innerHTML = "<h4><strong>Abstract</strong>:</h4>"+data.articles[n].description;
+            newsurl.innerHTML = '<strong>Source</strong>: <a href="'+data.articles[n].url + '" target="_blank">' + data.articles[n].source.name  + '</a>';
+            //number of random articles to choose from
+            console.log(`Articles fetched: ${numArticles}`);
+        })
+        .catch(err => console.log(err))
+    })
+    
+});
+
